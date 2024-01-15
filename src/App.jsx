b811943b6ee52b8f02cb1dc89f21c145bbe1667f
M-Feale <marjolainefeale@gmail.com>
2024-01-15 16:39:04 -0500

    updated-logic-for-time-setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
-	const [time, setTime] = useState(null);
 	const [seconds, setSeconds] = useState(0);
 	const [minutes, setMinutes] = useState(0);
 	const [hours, setHours] = useState(0);
 
-	// setTimeout(() => {
-	// 	setTime(Date.now());
-	// 	console.log(time);
-	// 	setSeconds(time / 1000);
-	// 	setMinutes(time / 1000 / 60);
-	// 	setHours(time / 1000 / 60 / 60);
-	// }, 1000);
+	const now = new Date()
+	console.log("this is now", now, typeof now, "ceci est le type ")
 
-  // Trying to figure out how time works and where I could get my seconds, minutes and hours from.
-  // Milliseconds in a day = 86,400,000
-  const date = new Date()
-  console.log("this is my date", date, typeof date, "ceci est le type ")
+	useEffect(() => {
+	setSeconds(now.getSeconds())
+	setMinutes(now.getMinutes())
+	setHours(now.getHours())
+	}, [])
 
-  const newTime = date.getTime();
-  console.log("this is the time ?", newTime, typeof newTime, "ceci est son type")
-
-  const newDate = Date(newTime)
-  console.log("this is newDate", newDate, typeof newDate, "ceci est son type")
-
+	const updateTime = () => {
+		// should I really create a function that sets time every 1000 seconds ?
+	}
 
 	return (
 		<>
-			<div>Time in ms = {time}</div>
-			<div>Time in second = {seconds}</div>
-      <div>Time in minutes = {minutes}</div>
-      <div>Time in hours = {hours}</div>
+		<div>Time in second = {seconds}</div>
+		<div>Time in minutes = {minutes}</div>
+		<div>Time in hours = {hours}</div>
 		</>
 	);
 };
