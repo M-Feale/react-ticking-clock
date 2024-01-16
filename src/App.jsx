abb33fc9c46f28ca713442cee1902157ee2beaf3
M-Feale <marjolainefeale@gmail.com>
2024-01-16 16:54:49 -0500

    implemented-conditional-rendering

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
 	const [hours, setHours] = useState(0);
 
 	let minuteDegrees = 180 + minutes * 6;
-	let hourDegrees = 180 + hours * 30 + (minutes/60 * 30)
+	let hourDegrees = 180 + hours * 30 + (minutes / 60) * 30;
 
 	const updateTime = () => {
 		const now = new Date();
@@ -16,28 +16,28 @@ const App = () => {
 
 		//Circle has 360 degrees meaning that every minute, the minute hand must move 6 degrees.
 		setMinutes(now.getMinutes());
-	
 
 		// Clock only displays 12 hours so every hour, the hour hand moves 30 degrees. Counterpoint, on a real clock, hour hand moves progressively...
 		setHours(now.getHours());
-		
 	};
-	
+
 	setInterval(updateTime, 1000);
 
 	return (
-		<>
-			<Box>
-				<div>Time in second = {seconds}</div>
-				<div>Time in minutes = {minutes}</div>
-				<div>Time in hours = {hours}</div>
-			</Box>
-			<img src={clock} />
-			<ClockFace>
-				<Hand className="minute" $travel={minuteDegrees}></Hand>
-				<Hand className="hour" $travel={hourDegrees}></Hand>
-			</ClockFace>
-		</>
+		minutes > 0 && (
+			<>
+				<Box>
+					<div>Time in second = {seconds}</div>
+					<div>Time in minutes = {minutes}</div>
+					<div>Time in hours = {hours}</div>
+				</Box>
+				<ClockFrame src={clock} />
+				<ClockFace>
+					<Hand className="minute" $travel={minuteDegrees}></Hand>
+					<Hand className="hour" $travel={hourDegrees}></Hand>
+				</ClockFace>
+			</>
+		)
 	);
 };
 
@@ -46,12 +46,19 @@ const Box = styled.div`
 	background-color: purple;
 `;
 
+const ClockFrame = styled.img`
+	position: relative;
+`;
+
 const ClockFace = styled.div`
-	height: 200px;
-	width: 200px;
-	background-color: black;
+	height: 100px;
+	width: 100px;
+	background-color: yellow;
 	position: relative;
 	border-radius: 50%;
+
+	top: -618px;
+	left: 97px;
 `;
 
 const Hand = styled.div`
@@ -62,12 +69,12 @@ const Hand = styled.div`
 	transform-origin: 50% 5px;
 
 	&.minute {
-		height: 100px;
+		height: 50px;
 		background-color: mediumaquamarine;
 		transform: ${(props) => `rotate(${props.$travel}deg)`};
 	}
 	&.hour {
-		height: 70px;
+		height: 35px;
 		background-color: hotpink;
 		transform: ${(props) => `rotate(${props.$travel}deg)`};
 	}
