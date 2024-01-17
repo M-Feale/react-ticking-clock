e96ee61218d27e7bf0fc3c77e3e5c817c84e9b93
M-Feale <marjolainefeale@gmail.com>
2024-01-17 11:54:29 -0500

    WIP-second-movement-logic

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,21 @@ const App = () => {
 	const [minutes, setMinutes] = useState(0);
 	const [hours, setHours] = useState(0);
 
+	// Starting at 0 degrees, travels 4 degrees one way in a half a second, hits the "second" at the apex and travels 8 degrees the other way in a second. 30 odd numbers, 30 even numbers between 0 and 59 (possible seconds). Even on the left, odd on the right ?
+	let secondDegrees;
+
+	//Circle has 360 degrees meaning that every minute, the minute hand must move 6 degrees.
 	let minuteDegrees = 180 + minutes * 6;
+	// Clock only displays 12 hours so every hour is 30 degrees. The hour hand then moves proportionally according to how many minutes have elapsed.
 	let hourDegrees = 180 + hours * 30 + (minutes / 60) * 30;
 
 	const updateTime = () => {
 		const now = new Date();
+
 		setSeconds(now.getSeconds());
 
-		//Circle has 360 degrees meaning that every minute, the minute hand must move 6 degrees.
 		setMinutes(now.getMinutes());
 
-		// Clock only displays 12 hours so every hour, the hour hand moves 30 degrees. Counterpoint, on a real clock, hour hand moves progressively...
 		setHours(now.getHours());
 	};
 
@@ -33,9 +37,12 @@ const App = () => {
 				</Box>
 				<ClockFrame>
 					<ClockImage src={clock} />
-					<SecondsHand></SecondsHand>
+					<SecondsHand
+						className="second"
+						$travel={secondDegrees}
+					></SecondsHand>
 				</ClockFrame>
-				
+
 				<ClockFace>
 					<Hand className="minute" $travel={minuteDegrees}></Hand>
 					<Hand className="hour" $travel={hourDegrees}></Hand>
@@ -68,7 +75,7 @@ const SecondsHand = styled.div`
 	top: 313px;
 	left: 143px;
 
-	transform: rotate(0deg);
+	transform: rotate(4deg);
 	transform-origin: 50% -70px;
 `;
 
