66273393dccd5237981833133d2c219c650261f7
M-Feale <marjolainefeale@gmail.com>
2024-01-19 16:09:40 -0500

    implemented-digital-clock-casing-and-display

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -3,19 +3,54 @@ import styled from "styled-components";
 const DigitalClock = ({ time }) => {
 	return (
 		time.seconds != null && (
-			<Box>
-				<div>Time in second = {time.seconds}</div>
-				<div>Time in minutes = {time.minutes}</div>
-				<div>Time in hours = {time.hours}</div>
-			</Box>
+			<Wrapper>
+				<Casing>
+					<Display>
+						<Time>{time.hours}</Time>
+						<Colon>:</Colon>
+						<Time>{time.minutes}</Time>
+						<Colon>:</Colon>
+						<Time>{time.seconds}</Time>
+						{/* {time.hours}:{time.minutes}:{time.seconds} */}
+					</Display>
+				</Casing>
+			</Wrapper>
 		)
 	);
 };
 
-const Box = styled.div`
-	color: white;
-	background-color: purple;
-    font-family: "Orbitron", sans-serif;
+const Casing = styled.div`
+	background-color: black;
+	width: 270px;
+	height: 100px;
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	border-radius: 16px;
+`;
+
+const Display = styled.div`
+	min-width: 80%;
+	font-family: "Orbitron", sans-serif;
+	font-size: 36px;
+	border-radius: 12px;
+	padding: 10px;
+	background-color: white;
+	color: black;
+
+	display: flex;
+	flex-direction: row;
+	justify-content: space-evenly;
+	align-items: center;
+`;
+
+const Time = styled.span`
+	width: 30%;
+`;
+
+const Colon = styled.span`
+	width: 5%;
 `;
 
 export default DigitalClock;
