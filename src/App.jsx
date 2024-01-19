eaadfb5ccb3bfa4479b2730286c25675ea96e99b
M-Feale <marjolainefeale@gmail.com>
2024-01-19 17:33:58 -0500

    ajusted-outermost-container

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import styled from "styled-components";
 import AnalogClock from "./AnalogClock";
 import DigitalClock from "./DigitalClock";
 
-
 const App = () => {
 	const [time, setTime] = useState({
 		seconds: null,
@@ -15,9 +14,15 @@ const App = () => {
 	const updateTime = () => {
 		const now = new Date();
 		setTime({
-			seconds: now.getSeconds().toLocaleString(undefined, {minimumIntegerDigits: 2}),
-			minutes: now.getMinutes().toLocaleString(undefined, {minimumIntegerDigits: 2}),
-			hours: now.getHours().toLocaleString(undefined, {minimumIntegerDigits: 2}),
+			seconds: now
+				.getSeconds()
+				.toLocaleString(undefined, { minimumIntegerDigits: 2 }),
+			minutes: now
+				.getMinutes()
+				.toLocaleString(undefined, { minimumIntegerDigits: 2 }),
+			hours: now
+				.getHours()
+				.toLocaleString(undefined, { minimumIntegerDigits: 2 }),
 		});
 	};
 
@@ -35,7 +40,7 @@ const TimeContainer = styled.div`
 	display: flex;
 	flex-direction: row;
 	justify-content: space-evenly;
-    max-height: 95vh;
+	align-items: center;
 `;
 
 export default App;
